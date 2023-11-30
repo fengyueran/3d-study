7afad803a7a47e9f65ad33c76903fedd8c3aeeba
xinghunm <xinghunm@curacloudcorp.com>
2023-11-30 23:21:21 +0800

    add examples

--- packages/study1/src/App.tsx
+++ packages/study1/src/App.tsx
@@ -1,20 +1,16 @@
-// import * as THREE from 'three';
 import styled from 'styled-components';
+
+import { chapter1 } from './chapters';
 
 const RootContainer = styled.div`
   width: 100vw;
   height: 100vh;
 `;
 
-const Canvas = styled.canvas`
-  width: 400px;
-  height: 400px;
-`;
-
 function App() {
   return (
     <RootContainer>
-      <Canvas />
+      <chapter1.OrbitControl />
     </RootContainer>
   );
 }
---
+++ packages/study1/src/chapters/index.ts
@@ -0,0 +1,1 @@
+export * as chapter1 from './01-three-basic';
