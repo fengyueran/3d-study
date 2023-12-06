a2ee38cd5fdfb81d57003196d13c75d7318f2ab3
xinghunm <316032603@qq.com>
2023-12-06 09:07:59 +0800

    enable to change lessons by route

--- packages/study1/src/chapters/index.ts
+++ packages/study1/src/chapters/index.ts
@@ -1,9 +1,9 @@
 import * as chapter1 from './01-three-basic';
 
 export const lessons = {
-  '0-4': chapter1.FirstScene,
-  '0-8': chapter1.Light,
-  '0-12': chapter1.SizeChange,
-  '0-13': chapter1.Stats,
-  '0-14': chapter1.PerspectiveCamera,
+  '1-04': chapter1.FirstScene,
+  '1-08': chapter1.Light,
+  '1-12': chapter1.SizeChange,
+  '1-13': chapter1.Stats,
+  '1-14': chapter1.PerspectiveCamera,
 };
--- packages/study1/src/features/catalogue/catalogue.tsx
+++ packages/study1/src/features/catalogue/catalogue.tsx
@@ -7,38 +7,38 @@ const { DirectoryTree } = Tree;
 const treeData: DataNode[] = [
   {
     title: '01、认识three.js与开发环境搭建',
-    key: '0',
+    key: '1',
     children: [
       {
         title: '04.创建3D场景',
-        key: '0-4',
+        key: '1-04',
         isLeaf: true,
       },
       {
         title: '08.光源对物体表面的影响',
-        key: '0-8',
+        key: '1-08',
         isLeaf: true,
       },
       {
         title: '12.Canvas画布大小变化',
-        key: '0-12',
+        key: '1-12',
         isLeaf: true,
       },
       {
         title: '13.Stats使用',
-        key: '0-13',
+        key: '1-13',
         isLeaf: true,
       },
       {
         title: '14.透视相机体验',
-        key: '0-14',
+        key: '1-14',
         isLeaf: true,
       },
     ],
   },
   {
     title: '02、Three.js开发入门与调试设置',
-    key: '0-1',
+    key: '2',
   },
 ];
 
