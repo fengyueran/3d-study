8e9e3ceb934323b23f4751a9a5988b5f2610d701
xinghunm <xinghunm@curacloudcorp.com>
2023-12-06 23:12:57 +0800

    update demo

--- packages/study1/src/chapters/01-three-basic/14.perspective-camera.tsx
+++ packages/study1/src/chapters/01-three-basic/14.perspective-camera.tsx
@@ -18,17 +18,30 @@ export const PerspectiveCamera = () => {
     const container = containerRef.current!;
 
     //https://threejs.org/examples/?q=webgl%20ca#webgl_camera
-    //4个参数可以决定视椎体的大小
+    //4个参数可以决定视椎体的大小，透视投影，远小近大
     const camera = new THREE.PerspectiveCamera(
-      45, //摄像机视锥体垂直视野角度
+      30, //摄像机视锥体垂直视野角度，角度越大椎体体积越大，物体相对就会更小
       container.clientWidth / container.clientHeight,
       0.1,
-      1000
+      3000
     );
 
-    camera.position.set(200, 200, 200);
-    camera.lookAt(0, 0, 0);
+    // const camera = new THREE.PerspectiveCamera(
+    //   30, //摄像机视锥体垂直视野角度
+    //   container.clientWidth / container.clientHeight,
+    //   0.1,
+    //   8000
+    // );
 
+    // camera.position.set(200, 200, 200);
+    //相机越远，看得越多，想象拍照
+    // camera.position.set(800, 800, 800);
+
+    //相机挪得太远，远平面外的物体不会显示，相机位置的挪动不会改变视椎体的大小
+    camera.position.set(2000, 2000, 2000);
+    camera.lookAt(0, 0, 0);
+    //相机对准哪个物体，哪个物体就在画布的中间
+    // camera.lookAt(1000, 0, 1000);
     scene.add(camera);
 
     const cubeGeometry = new THREE.BoxGeometry(100, 100, 100);
@@ -52,7 +65,7 @@ export const PerspectiveCamera = () => {
     scene.add(axesHelper);
 
     const pointLight = new THREE.PointLight(0xffffff, 100000);
-    pointLight.position.set(50, 50, 50);
+    pointLight.position.set(400, 200, 300);
     scene.add(pointLight);
 
     const pointLightHelper = new THREE.PointLightHelper(pointLight, 4);
@@ -67,7 +80,11 @@ export const PerspectiveCamera = () => {
 
     container.appendChild(webGLRenderer.domElement);
 
+    //OrbitControls会影响相机lookAt值
     const orbitControls = new OrbitControls(camera, webGLRenderer.domElement);
+
+    // orbitControls.target.set(1000, 0, 1000);
+    // orbitControls.update();
 
     function render() {
       requestAnimationFrame(render);
