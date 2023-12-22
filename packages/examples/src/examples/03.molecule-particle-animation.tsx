98e021e48f455cce75af4d62bf70a96bd475a0fd
xinghunm <316032603@qq.com>
2023-12-22 15:30:55 +0800

    update

--- packages/examples/src/examples/03.molecule-particle-animation.tsx
+++ packages/examples/src/examples/03.molecule-particle-animation.tsx
@@ -2,7 +2,7 @@ import { useRef, useEffect } from 'react';
 import * as THREE from 'three';
 import styled from 'styled-components';
 
-import { TrackballControls } from 'three/examples/jsm/controls/TrackballControls';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 import { PDBLoader } from 'three/examples/jsm/loaders/PDBLoader';
 import {
@@ -12,134 +12,42 @@ import {
 import { GUI } from 'dat.gui';
 import TWEEN from 'three/examples/jsm/libs/tween.module.js';
 
-import Al2O3 from './Al2O3.pdb';
-import ethanol from './ethanol.pdb';
+import Al2O3 from './pdb/Al2O3.pdb';
+import aspirin from './pdb/aspirin.pdb';
+import buckyball from './pdb/buckyball.pdb';
+import caf2 from './pdb/caf2.pdb';
+import caffeine from './pdb/caffeine.pdb';
+import cholesterol from './pdb/cholesterol.pdb';
+import cocaine from './pdb/cocaine.pdb';
+import cu from './pdb/cu.pdb';
+import ethanol from './pdb/ethanol.pdb';
 
 const gui = new GUI();
 
 const Container = styled.div`
   height: 100%;
 `;
+const getRandom = () => Math.random() * 300 - 200;
 
-export const MoleculeParticleAnimation = () => {
-  const containerRef = useRef<HTMLDivElement>(null);
-
-  useEffect(() => {
-    const container = containerRef.current!;
-
-    let labelRenderer;
-    let controls;
-
-    const scene = new THREE.Scene();
-    const root = new THREE.Group();
-
-    scene.background = new THREE.Color(0x050505);
-
-    const camera = new THREE.PerspectiveCamera(
-      70,
-      window.innerWidth / window.innerHeight,
-      1,
-      5000
-    );
-    camera.position.z = 1000;
-    scene.add(camera);
-
-    const renderer = new THREE.WebGLRenderer({ antialias: true });
-    const MOLECULES = {
-      Ethanol: 'ethanol.pdb',
-      Aspirin: 'aspirin.pdb',
-      Caffeine: 'caffeine.pdb',
-      Nicotine: 'nicotine.pdb',
-      LSD: 'lsd.pdb',
-      Cocaine: 'cocaine.pdb',
-      Cholesterol: 'cholesterol.pdb',
-      Lycopene: 'lycopene.pdb',
-      Glucose: 'glucose.pdb',
-      'Aluminium oxide': 'Al2O3.pdb',
-      Cubane: 'cubane.pdb',
-      Copper: 'cu.pdb',
-      Fluorite: 'caf2.pdb',
-      Salt: 'nacl.pdb',
-      'YBCO superconductor': 'ybco.pdb',
-      Buckyball: 'buckyball.pdb',
-      Graphite: 'graphite.pdb',
-    };
-
-    const params = {
-      molecule: 'caffeine.pdb',
-    };
-
-    const loader = new PDBLoader();
-    const offset = new THREE.Vector3();
-
-    init();
-
-    function init() {
-      const light1 = new THREE.DirectionalLight(0xffffff, 2.5);
-      light1.position.set(1, 1, 1);
-      scene.add(light1);
-
-      const light2 = new THREE.DirectionalLight(0xffffff, 1.5);
-      light2.position.set(-1, -1, 1);
-      scene.add(light2);
-
-      scene.add(root);
-
-      renderer.setPixelRatio(window.devicePixelRatio);
-      renderer.setSize(container.clientWidth, container.clientHeight);
-      container.appendChild(renderer.domElement);
-
-      // labelRenderer = new CSS2DRenderer();
-      // labelRenderer.setSize(container.clientWidth, container.clientHeight);
-      // labelRenderer.domElement.style.position = 'absolute';
-      // labelRenderer.domElement.style.top = '0px';
-      // labelRenderer.domElement.style.pointerEvents = 'none';
-
-      // container.appendChild(labelRenderer.domElement);
-
-      // controls = new TrackballControls(camera, renderer.domElement);
-      // controls.minDistance = 500;
-      // controls.maxDistance = 2000;
-
-      //
-
-      loadMolecule(params.molecule);
-
-      gui.add(params, 'molecule', MOLECULES).onChange(loadMolecule);
-      gui.open();
-    }
-
-    const lines = new THREE.Group();
-    const atoms = new THREE.Group();
-    const atomArray = [];
-    const atomPositions = [];
-
-    const particlesTotal = 9;
-    const animationTypeCount = 2;
-    let current = 0;
-
-    for (let i = 0; i < particlesTotal; i++) {
-      atomPositions.push(
-        Math.random() * 300 - 200,
-        Math.random() * 300 - 200,
-        Math.random() * 300 - 200
-      );
-    }
-
-    function loadMolecule(model) {
-      const url = ethanol;
-
-      while (root.children.length > 0) {
-        const object = root.children[0];
-        object.parent.remove(object);
-      }
+const loadMolecule = (
+  url: string
+): Promise<{ atoms: THREE.Mesh[]; lines: THREE.Mesh[] }> =>
+  new Promise((reslove) => {
+    {
+      const loader = new PDBLoader();
+      const offset = new THREE.Vector3();
+      const lines = [] as THREE.Mesh[];
+      const atoms = [] as THREE.Mesh[];
+      // while (root.children.length > 0) {
+      //   const object = root.children[0];
+      //   object.parent.remove(object);
+      // }
 
       loader.load(url, function (pdb) {
         const geometryAtoms = pdb.geometryAtoms;
         const geometryBonds = pdb.geometryBonds;
         // const json = pdb.json;
 
-        const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
         const sphereGeometry = new THREE.IcosahedronGeometry(1, 3);
 
         geometryAtoms.computeBoundingBox();
@@ -149,7 +57,7 @@ export const MoleculeParticleAnimation = () => {
         geometryBonds.translate(offset.x, offset.y, offset.z);
 
         let positions = geometryAtoms.getAttribute('position');
-        // const colors = geometryAtoms.getAttribute('color');
+        const colors = geometryAtoms.getAttribute('color');
 
         const position = new THREE.Vector3();
         const color = new THREE.Color();
@@ -159,9 +67,9 @@ export const MoleculeParticleAnimation = () => {
           position.y = positions.getY(i);
           position.z = positions.getZ(i);
 
-          // color.r = colors.getX(i);
-          // color.g = colors.getY(i);
-          // color.b = colors.getZ(i);
+          color.r = colors.getX(i);
+          color.g = colors.getY(i);
+          color.b = colors.getZ(i);
 
           const material = new THREE.MeshPhongMaterial({ color: color });
 
@@ -169,17 +77,8 @@ export const MoleculeParticleAnimation = () => {
           object.position.copy(position);
           object.position.multiplyScalar(75);
           object.scale.multiplyScalar(25);
-          Math.random() * 300 - 200,
-            atomPositions.push(
-              object.position.x,
-              object.position.y,
-              object.position.z
-            );
-          object.position.x = Math.random() * 300 - 200;
-          object.position.y = Math.random() * 300 - 200;
-          object.position.z = Math.random() * 300 - 200;
-          atoms.add(object);
-          atomArray.push(object);
+
+          atoms.push(object);
 
           // const atom = json.atoms[i];
 
@@ -193,13 +92,14 @@ export const MoleculeParticleAnimation = () => {
           // label.position.copy(object.position);
           // root.add(label);
         }
-        root.add(atoms);
+        // root.add(atoms);
 
         positions = geometryBonds.getAttribute('position');
 
         const start = new THREE.Vector3();
         const end = new THREE.Vector3();
 
+        const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
         for (let i = 0; i < positions.count; i += 2) {
           start.x = positions.getX(i);
           start.y = positions.getY(i);
@@ -224,68 +124,227 @@ export const MoleculeParticleAnimation = () => {
           object.position.lerp(end, 0.5);
           object.scale.set(5, 5, start.distanceTo(end));
           object.lookAt(end);
-          lines.add(object);
+          lines.push(object);
         }
-        root.add(lines);
-        transition();
+
+        reslove({ atoms, lines });
       });
     }
+  });
 
-    const transition = () => {
-      const offset = current * particlesTotal * 3;
-      const duration = 3000;
+export const MoleculeParticleAnimation = () => {
+  const containerRef = useRef<HTMLDivElement>(null);
 
-      for (let i = 0, j = offset; i < particlesTotal; i++, j += 3) {
-        const object = atomArray[i];
-        // debugger; //eslint-disable-line
+  useEffect(() => {
+    const start = async () => {
+      const container = containerRef.current!;
 
-        const positionTween = new TWEEN.Tween(object.position)
-          .to(
-            {
-              x: atomPositions[j],
-              y: atomPositions[j + 1],
-              z: atomPositions[j + 2],
-            },
-            duration
-          )
-          .easing(TWEEN.Easing.Exponential.InOut);
+      let labelRenderer;
+      let controls;
 
-        const start = current % animationTypeCount === 0 ? 0 : 1;
-        const end = start === 1 ? 0 : 1;
-        // console.log('start', start);
-        // console.log('end', end);
+      const scene = new THREE.Scene();
+      const root = new THREE.Group();
 
-        const opacityTween = new TWEEN.Tween({ opacity: start })
-          .to({ opacity: end }, 1000)
-          .onUpdate((obj) => {
-            lines.traverse((line) => {
-              if (line.material) {
-                line.material.opacity = obj.opacity;
-              }
-            });
+      scene.background = new THREE.Color(0x050505);
+
+      const camera = new THREE.PerspectiveCamera(
+        70,
+        container.clientWidth / container.clientHeight,
+        1,
+        5000
+      );
+      camera.position.z = 1000;
+      scene.add(camera);
+
+      const renderer = new THREE.WebGLRenderer({ antialias: true });
+      const MOLECULES = [
+        { url: ethanol, name: 'ethanol', count: 9, offset: -200 },
+        // { url: Al2O3, name: 'Al2O3', count: 9 },
+        { url: aspirin, name: 'aspirin', count: 21, offset: 300 },
+        // { url: caf2, name: 'caf2', count: 22, offset: -500 },
+        // { url: cu, name: 'cu', count: 45, offset: 0 },
+        // { url: Al2O3, name: 'Al2O3' },
+        // { url: Al2O3, name: 'Al2O3' },
+        // { url: Al2O3, name: 'Al2O3' },
+        // { url: Al2O3, name: 'Al2O3' },
+        // { url: Al2O3, name: 'Al2O3' },
+      ];
+
+      const params = {
+        molecule: 'caffeine.pdb',
+      };
+
+      const atomPositions: number[] = [];
+
+      const particlesTotal = MOLECULES.reduce((p, c) => {
+        return p + c.count;
+      }, 0);
+      const animationTypeCount = 3;
+      let current = 1;
+
+      // for (let i = 0; i < particlesTotal; i++) {
+      //   atomPositions.push(getRandom(), getRandom(), getRandom() - offset);
+      // }
+
+      const opacitys = [
+        { start: 0, end: 0 },
+        { start: 0, end: 1 },
+        { start: 1, end: 0 },
+      ];
+      const transition = (atoms: THREE.Mesh[], lines: THREE.Mesh[]) => {
+        const offset = current * particlesTotal * 3;
+        const duration = 3000;
+
+        const currentOpacity = opacitys[current];
+
+        for (let i = 0, j = offset; i < particlesTotal; i++, j += 3) {
+          const object = atoms[i];
+
+          const positionTween = new TWEEN.Tween({
+            x: object.position.x,
+            y: object.position.y,
+            z: object.position.z,
+            opacity: currentOpacity.start,
           })
-          .easing(TWEEN.Easing.Exponential.InOut);
+            .to(
+              {
+                x: atomPositions[j],
+                y: atomPositions[j + 1],
+                z: atomPositions[j + 2],
+                opacity: currentOpacity.end,
+              },
+              duration
+            )
+            .onUpdate((obj) => {
+              object.position.x = obj.x;
+              object.position.y = obj.y;
+              object.position.z = obj.z;
+              lines.forEach((line) => {
+                if (line.material) {
+                  line.material.opacity = obj.opacity;
+                }
+              });
+            })
+            .easing(TWEEN.Easing.Exponential.InOut);
 
-        positionTween.chain(opacityTween);
-        positionTween.start();
-      }
+          positionTween.start();
+        }
 
-      new TWEEN.Tween(this).to({}, 2000).onComplete(transition).start();
-      current = (current + 1) % animationTypeCount;
+        new TWEEN.Tween(this)
+          .to({}, 4000)
+          .onComplete(() => transition(atoms, lines))
+          .start();
+        current = (current + 1) % animationTypeCount;
+      };
+
+      const init = async () => {
+        const light1 = new THREE.DirectionalLight(0xffffff, 2.5);
+        light1.position.set(1, 1, 1);
+        scene.add(light1);
+
+        const light2 = new THREE.DirectionalLight(0xffffff, 1.5);
+        light2.position.set(-1, -1, 1);
+        scene.add(light2);
+
+        scene.add(root);
+
+        renderer.setPixelRatio(window.devicePixelRatio);
+        renderer.setSize(container.clientWidth, container.clientHeight);
+        container.appendChild(renderer.domElement);
+
+        // labelRenderer = new CSS2DRenderer();
+        // labelRenderer.setSize(container.clientWidth, container.clientHeight);
+        // labelRenderer.domElement.style.position = 'absolute';
+        // labelRenderer.domElement.style.top = '0px';
+        // labelRenderer.domElement.style.pointerEvents = 'none';
+
+        // container.appendChild(labelRenderer.domElement);
+
+        // controls = new TrackballControls(camera, renderer.domElement);
+        // controls.minDistance = 500;
+        // controls.maxDistance = 2000;
+
+        const allAtoms = [];
+        const allLines = [];
+
+        for (let i = 0; i < MOLECULES.length; i += 1) {
+          const cur = MOLECULES[i];
+          const offset = cur.offset || 0;
+          for (let i = 0; i < cur.count; i += 1) {
+            atomPositions.push(getRandom() + offset, getRandom(), getRandom());
+          }
+        }
+        let startIndex = 0;
+        for (let i = 0; i < MOLECULES.length; i += 1) {
+          const cur = MOLECULES[i];
+          const { atoms, lines } = await loadMolecule(cur.url);
+
+          const offset = cur.offset || 0;
+
+          for (let j = 0; j < atoms.length; j += 1) {
+            const atom = atoms[j];
+            atomPositions.push(
+              atom.position.x + offset,
+              atom.position.y,
+              atom.position.z
+            );
+            atom.position.x = atomPositions[startIndex + j];
+            atom.position.y = atomPositions[startIndex + j + 1];
+            atom.position.z = atomPositions[startIndex + j + 2];
+
+            root.add(atom);
+          }
+
+          lines.forEach((line) => {
+            line.position.x += offset;
+
+            root.add(line);
+          });
+
+          allAtoms.push(...atoms);
+          allLines.push(...lines);
+          startIndex += cur.count;
+        }
+
+        const radius = 275;
+
+        for (let i = 0; i < particlesTotal; i++) {
+          const phi = Math.acos(-1 + (2 * i) / particlesTotal);
+          const theta = Math.sqrt(particlesTotal * Math.PI) * phi;
+
+          atomPositions.push(
+            radius * Math.cos(theta) * Math.sin(phi),
+            radius * Math.sin(theta) * Math.sin(phi),
+            radius * Math.cos(phi)
+          );
+        }
+
+        transition(allAtoms, allLines);
+        gui.add(params, 'molecule', MOLECULES).onChange(loadMolecule);
+        gui.open();
+      };
+
+      const axesHelper = new THREE.AxesHelper(400);
+      scene.add(axesHelper);
+
+      const orbitControls = new OrbitControls(camera, renderer.domElement);
+
+      await init();
+
+      renderer.setAnimationLoop(() => {
+        // controls.update();
+
+        TWEEN.update();
+        const time = Date.now() * 0.0004;
+
+        root.rotation.x = time;
+        root.rotation.y = time * 0.7;
+
+        renderer.render(scene, camera);
+        // labelRenderer.render(scene, camera);
+      });
     };
-
-    renderer.setAnimationLoop(() => {
-      // controls.update();
-
-      TWEEN.update();
-      const time = Date.now() * 0.0004;
-
-      root.rotation.x = time;
-      root.rotation.y = time * 0.7;
-
-      renderer.render(scene, camera);
-      // labelRenderer.render(scene, camera);
-    });
+    start();
   }, []);
 
   return <Container ref={containerRef} />;
